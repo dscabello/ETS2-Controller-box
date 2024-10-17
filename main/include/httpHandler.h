0aaaa3174d816b5cde17c37ce0863f5d9f2373b5
Danilo Peixoto <dscabello@gmail.com>
2024-10-18 00:01:11 +0200

    Adding converter map from Json to var

--- main/include/httpHandler.h
+++ main/include/httpHandler.h
@@ -11,6 +11,49 @@
 #include "ets2ctrl.h"
 #include "wifiHandler.h"
 
+
+enum Types_e {
+    NOT_DEFINED     = 0,
+    BOOL_TYPE       = 1,
+    INT32_TYPE      = 2,
+    UINT32_TYPE     = 3
+};
+
+typedef struct {
+    const char *group;
+    const char *name;
+    void *varPointer;
+    enum Types_e types;       // Tipo da variável ('i' para int, 'b' para bool)
+} VarMap_t;
+
+extern bool GameInfo_connected;
+
+extern int32_t TruckInfo_gear; // TODO: Verify if we have negative values
+extern bool TruckInfo_cruiseControlOn;
+extern bool TruckInfo_engineOn;
+extern bool TruckInfo_wisperOn;
+extern bool TruckInfo_engineOn;
+extern bool TruckInfo_blinkerLeftActive;
+extern bool TruckInfo_blinkerRightActive;
+extern bool TruckInfo_blinkerLeftOn;
+extern bool TruckInfo_blinkerRightOn;
+extern bool TruckInfo_lightsParkingOn;
+extern bool TruckInfo_lightsBeamLowOn;
+extern bool TruckInfo_lightsBeamHighOn;
+extern bool TruckInfo_lightsAuxFrontOn;
+extern bool TruckInfo_lightsAuxRoofOn;
+extern bool TruckInfo_lightsBeaconOn;
+extern bool TruckInfo_lightsBrakeOn;
+extern bool TruckInfo_lightsDashboardOn;
+
+extern bool TrailerInfo_attached;
+
+extern uint32_t JobInfo_income;
+
+extern uint32_t NavegationInfo_estimatedDistance;
+
+
+
 extern bool RequestData;
 
 extern void HttpMainTask(void *pvParameters);
--- main/src/httpHandler.c
+++ main/src/httpHandler.c
@@ -4,6 +4,64 @@
 #define HTTP_SERVER_URL         CONFIG_HTTP_SERVER_URL
 #define BUFFER_SIZE             CONFIG_BUFFER_SIZE
 
+
+/* Vars from Http server */
+/* Game */
+bool GameInfo_connected;
+
+/* Truck */
+int32_t TruckInfo_gear;
+bool TruckInfo_cruiseControlOn;
+bool TruckInfo_wipersOn;
+bool TruckInfo_engineOn;
+bool TruckInfo_blinkerLeftActive;
+bool TruckInfo_blinkerRightActive;
+bool TruckInfo_blinkerLeftOn;
+bool TruckInfo_blinkerRightOn;
+bool TruckInfo_lightsParkingOn;
+bool TruckInfo_lightsBeamLowOn;
+bool TruckInfo_lightsBeamHighOn;
+bool TruckInfo_lightsAuxFrontOn;
+bool TruckInfo_lightsAuxRoofOn;
+bool TruckInfo_lightsBeaconOn;
+bool TruckInfo_lightsBrakeOn;
+bool TruckInfo_lightsDashboardOn;
+
+/* Trailer */
+bool TrailerInfo_attached;
+
+/* Job */
+uint32_t JobInfo_income;
+
+/* Navegation */
+uint32_t NavegationInfo_estimatedDistance;
+
+/* var_map used to link json and vars */
+VarMap_t var_map[] = {
+    { "game"        , "connected"         , &GameInfo_connected                 , BOOL_TYPE     },
+    { "truck"       , "gear"              , &TruckInfo_gear                     , INT32_TYPE    },
+    { "truck"       , "cruiseControlOn"   , &TruckInfo_cruiseControlOn          , BOOL_TYPE     },
+    { "truck"       , "wipersOn"          , &TruckInfo_wipersOn                 , BOOL_TYPE     },
+    { "truck"       , "engineOn"          , &TruckInfo_engineOn                 , BOOL_TYPE     },
+    { "truck"       , "blinkerLeftActive" , &TruckInfo_blinkerLeftActive        , BOOL_TYPE     },
+    { "truck"       , "blinkerRightActive", &TruckInfo_blinkerRightActive       , BOOL_TYPE     },
+    { "truck"       , "blinkerLeftOn"     , &TruckInfo_blinkerLeftOn            , BOOL_TYPE     },
+    { "truck"       , "blinkerRightOn"    , &TruckInfo_blinkerRightOn           , BOOL_TYPE     },
+    { "truck"       , "lightsParkingOn"   , &TruckInfo_lightsParkingOn          , BOOL_TYPE     },
+    { "truck"       , "lightsBeamLowOn"   , &TruckInfo_lightsBeamLowOn          , BOOL_TYPE     },
+    { "truck"       , "lightsBeamHighOn"  , &TruckInfo_lightsBeamHighOn         , BOOL_TYPE     },
+    { "truck"       , "lightsAuxFrontOn"  , &TruckInfo_lightsAuxFrontOn         , BOOL_TYPE     },
+    { "truck"       , "lightsAuxRoofOn"   , &TruckInfo_lightsAuxRoofOn          , BOOL_TYPE     },
+    { "truck"       , "lightsBeaconOn"    , &TruckInfo_lightsBeaconOn           , BOOL_TYPE     },
+    { "truck"       , "lightsBrakeOn"     , &TruckInfo_lightsBrakeOn            , BOOL_TYPE     },
+    { "truck"       , "lightsDashboardOn" , &TruckInfo_lightsDashboardOn        , BOOL_TYPE     },
+    { "trailer"     , "attached"          , &TrailerInfo_attached               , BOOL_TYPE     },
+    { "job"         , "income"            , &JobInfo_income                     , UINT32_TYPE   },
+    { "navegation"  , "estimatedDistance" , &NavegationInfo_estimatedDistance   , UINT32_TYPE   }
+};
+
+uint8_t var_map_size = sizeof(var_map) / sizeof(VarMap_t);
+
 bool RequestData = true;
 
 int JsonBufPOS=0;
@@ -11,13 +69,63 @@ char JsonBuf[BUFFER_SIZE];
 
 bool WipersOn = false;
 
+void ETS2_setVar(cJSON* cJSON_item_ptr, VarMap_t* var_map_ptr) {
+    if (var_map_ptr->types == BOOL_TYPE) {
+        if (cJSON_IsBool(cJSON_item_ptr)) {
+            bool aux_bool = cJSON_IsTrue(cJSON_item_ptr);
+            *((bool*)var_map_ptr->varPointer) = aux_bool;
+            ESP_LOGI(TAG,"Decode group = \"%s\", item = \"%s\", BOOL_TYPE = %d.", var_map_ptr->group, var_map_ptr->name, aux_bool);
+        } else {
+            ESP_LOGE(TAG,"Error: Decoding group = \"%s\", item = \"%s\" as a BOOL_TYPE", var_map_ptr->group, var_map_ptr->name);
+        }
+    } else if (var_map_ptr->types == INT32_TYPE) {
+        if (cJSON_IsNumber(cJSON_item_ptr)) {
+            int32_t aux_number = (int32_t)(cJSON_GetNumberValue(cJSON_item_ptr));
+            *((int32_t*)var_map_ptr->varPointer) = aux_number;
+            ESP_LOGI(TAG,"Decode group = \"%s\", item = \"%s\", INT32_TYPE = %" PRId32, var_map_ptr->group, var_map_ptr->name, aux_number);
+        } else {
+            ESP_LOGE(TAG,"Error: Decoding group = \"%s\", item = \"%s\" as a INT32_TYPE", var_map_ptr->group, var_map_ptr->name);
+        }
+    } else if (var_map_ptr->types == UINT32_TYPE) {
+        if (cJSON_IsNumber(cJSON_item_ptr)) {
+            uint32_t aux_number = (uint32_t)(cJSON_GetNumberValue(cJSON_item_ptr));
+            *((uint32_t*)var_map_ptr->varPointer) = aux_number;
+            ESP_LOGI(TAG,"Decode group = \"%s\", item = \"%s\", INT32_TYPE = %" PRId32, var_map_ptr->group, var_map_ptr->name, aux_number);
+        } else {
+            ESP_LOGE(TAG,"Error: Decoding group = \"%s\", item = \"%s\" as a UINT32_TYPE", var_map_ptr->group, var_map_ptr->name);
+        }
+    } else {
+        ESP_LOGE(TAG,"Error: no valid type.");
+    }
+}
+
+void ETS2_interpreter(cJSON* cJSON_ptr) {
+    uint32_t index;
+    ESP_LOGD(TAG, "ETS2_interpreter: Get data from JSON.");
+    if (cJSON_ptr == NULL) {
+        ESP_LOGE(TAG, "Error decoding JSON data");
+        return;
+    } else {
+        for (index = 0; index < var_map_size ; index++) {
+            cJSON *ItemGroup = cJSON_GetObjectItem(cJSON_ptr, var_map[index].group);
+            if (cJSON_IsObject(ItemGroup)) {
+                cJSON *Item = cJSON_GetObjectItem(ItemGroup, var_map[index].name);
+                if (Item == NULL)
+                    ESP_LOGE(TAG,"Not find group = %s, item = %s.", var_map[index].group, var_map[index].name);
+                else
+                    ETS2_setVar(Item, &var_map[index]);
+            }
+        }
+    }
+}
+
 esp_err_t HttpEventHandler(esp_http_client_event_t *evt) {
     switch (evt->event_id) {
         case HTTP_EVENT_ERROR:
-            ESP_LOGI(TAG, "HTTP_EVENT_ERROR");
+            // ESP_LOGI(TAG, "HTTP_EVENT_ERROR");
             break;
         case HTTP_EVENT_ON_CONNECTED:
-            ESP_LOGI(TAG, "HTTP_EVENT_ON_CONNECTED");
+            // ESP_LOGI(TAG, "HTTP_EVENT_ON_CONNECTED");
             break;
         case HTTP_EVENT_ON_HEADER:
             ESP_LOGI(TAG, "HTTP_EVENT_ON_HEADER: %.*s", evt->data_len, (char*)evt->data);
@@ -42,37 +150,23 @@ esp_err_t HttpEventHandler(esp_http_client_event_t *evt) {
                 ESP_LOGI(TAG, "Erro ao decodificar JSON");
                 ESP_LOGI(TAG, "Received data: %s", JsonBuf);
             } else {
-                cJSON *ItemTruck = cJSON_GetObjectItem(root, "truck");
-                cJSON *ItemWipersOn = cJSON_GetObjectItem(ItemTruck, "wipersOn");
-                if (cJSON_IsNumber(ItemWipersOn)) {
-                    ESP_LOGI(TAG, "Is a number");
-                    // WipersOn = ItemWipersOn->valueint;
-                    // ESP_LOGI(TAG, "\nDisplay Gear = %d.\n", WipersOn);
-                } else if (cJSON_IsBool(ItemWipersOn)) {
-                    WipersOn = cJSON_IsTrue(ItemWipersOn);
-                    // WipersOn = ItemWipersOn->valueint;
-                    ESP_LOGI(TAG, "\nDisplay Gear = %d.\n", WipersOn);
-                    ESP_LOGI(TAG, "Is a bool");
-                } else {
-                    ESP_LOGI(TAG, "ERROR is not a number");
-                }
-                // int displayedGear = cJSON_GetObjectItem(root, "displayedGear")->valueint;
+                ETS2_interpreter(root);
             }
 
             ESP_LOGI(TAG, "HTTP_EVENT_ON_FINISH");
             JsonBufPOS = 0;
             break;
         case HTTP_EVENT_DISCONNECTED:
-            ESP_LOGI(TAG, "HTTP_EVENT_ON_DISCONNECTED");
+            // ESP_LOGI(TAG, "HTTP_EVENT_ON_DISCONNECTED");
             break;
         case HTTP_EVENT_REDIRECT:
-            ESP_LOGI(TAG, "HTTP_EVENT_REDIRECT");
+            // ESP_LOGI(TAG, "HTTP_EVENT_REDIRECT");
             break;
         case HTTP_EVENT_HEADERS_SENT:
-            ESP_LOGI(TAG, "HTTP_EVENT_HEADERS_SENT");
+            // ESP_LOGI(TAG, "HTTP_EVENT_HEADERS_SENT");
             break;
         default:
-            ESP_LOGI(TAG, "Unhandled event: %d", evt->event_id);
+            // ESP_LOGI(TAG, "Unhandled event: %d", evt->event_id);
             break;
     }
     return ESP_OK;
